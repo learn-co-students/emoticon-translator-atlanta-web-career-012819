5724c3859873766e6c4fc47bde460a9e1417a891
Dana Simmons <dana@simmons-audio.com>
2019-01-27 17:34:29 -0500

    defined load library for YAML

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
-
-def load_library
-  # code goes here
+require 'yaml'
+def load_library(file_path)
+  face_hash = YAML.load_file(file_path)
+  lookup_hash = {get_meaning: {}, get_emoticon: {}}
+  face_hash.each do |meaning, emoji_pair| # [0:english,1:japanese]
+    lookup_hash[:get_meaning][emoji_pair[1]] = meaning
+    lookup_hash[:get_emoticon][emoji_pair[0]] = emoji_pair[1]
+  end
+  lookup_hash
 end
 
 def get_japanese_emoticon
